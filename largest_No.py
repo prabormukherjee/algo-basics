2839c345c34a8b4b954cb68911bbd156c9fe1ada
Prabor Mukherjee <prabor1@gmail.com>
2021-10-01 10:40:27 +0530

    fixed input issue

--- largest_No.py
+++ largest_No.py
@@ -2,9 +2,9 @@
 
 
 # uncomment following lines to take three numbers from user
-#num1 = float(input("Enter first number: "))
-#num2 = float(input("Enter second number: "))
-#num3 = float(input("Enter third number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
